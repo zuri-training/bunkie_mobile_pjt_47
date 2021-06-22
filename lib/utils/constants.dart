11bbbcb7976cbdd4d860dba1a178a2e6ec1f29ce
Arch997 <archibongc4@gmail.com>
2021-06-22 13:15:57 +0100

    Bug: Resolved merge conflicts

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,13 +1,14 @@
 const SplashScreenViewRoute = '/';
-const ProfileCreateViewRoute = 'create-profile';
-const SelectionViewRoute = 'select';
+
 const LoginViewRoute = 'login-view';
 const LandingViewRoute = 'landing-page';
 const SignUpViewRoute = 'sign-up-view';
 const VerifyEmailViewRoute = 'verify-email-view';
 const VerifyNumberViewRoute = 'verify-number-view';
 const CodeVerificationViewRoute = 'code-verification-view';
+const ProfileCreateViewRoute = 'create-profile';
 const AmenitiesViewRoute = 'amenities-view';
+const SelectionViewRoute = 'select';
 const LookingForRoommateViewRoute = 'looking-for-roommate-view';
 const LookingForApartmentViewRoute = 'looking-for-apartment-view';
 const InterestsAndLifestyleViewRoute = 'interests-and-lifestyle-view';
