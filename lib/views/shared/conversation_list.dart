502043c7a797db2548996e9ae3ebe2f3776de37e
Chukwuyenum Adeshina <adeshina.jude@gmail.com>
2021-07-08 11:56:49 +0100

    -Improved-chatpage-view-UI-and-chatdetail-view-UI-

--- lib/views/shared/conversation_list.dart
+++ lib/views/shared/conversation_list.dart
@@ -17,94 +17,96 @@ class ConversationList extends StatefulWidget {
   String? lastMessage;
   QueryDocumentSnapshot contact;
 
-  ConversationList({
-    required this.name, 
-    this.messageContent, 
-    this.imageUrl,
-    this.time,
-    required this.messageRead,
-    required this.uid,
-    required this.pid,
-    required this.contact,
-    this.lastMessage
-  });
+  ConversationList(
+      {required this.name,
+      this.messageContent,
+      this.imageUrl,
+      this.time,
+      required this.messageRead,
+      required this.uid,
+      required this.pid,
+      required this.contact,
+      this.lastMessage});
   @override
   _ConversationListState createState() => _ConversationListState();
 }
-
 
 class _ConversationListState extends State<ConversationList> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => createConversation(context),
-      child: Container(
-        padding: EdgeInsets.only(
-          left: 16,
-          right: 16,
-          top: 10,
-          bottom: 10
-        ),
-        child: Row(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      widget.imageUrl ?? 'https://i.pravatar.cc/150?img=1',
-                    ),
-                    maxRadius: 30,
-                  ),
-                  SizedBox(width: 16),
-                  Expanded(
-                    child: Container(
-                      color: Colors.transparent,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(widget.name, style: GoogleFonts.cabin(
-                            fontSize: 16,
-                            color: Colors.black
-                          )),
-                          SizedBox(height: 6),
-                          Text(
-                            widget.messageContent as String,
-                            style: GoogleFonts.cabin(
-                              fontSize: 13,
-                              color: Colors.black,
-                              fontWeight: widget.messageRead ? FontWeight.bold : FontWeight.normal
-                            ),
-                          )
-                        ]
+        onTap: () => createConversation(context),
+        child: Container(
+            padding: EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          widget.imageUrl ?? 'https://i.pravatar.cc/150?img=1',
+                        ),
+                        maxRadius: 26,
                       ),
-                    )
+                      SizedBox(width: 16),
+                      Expanded(
+                          child: Container(
+                        color: Colors.transparent,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(widget.name,
+                                  style: GoogleFonts.cabin(
+                                      fontSize: 16, color: Colors.black)),
+                              SizedBox(height: 6),
+                              Container(
+                                width: 220,
+                                child: Row(
+                                  children: [
+                                    Flexible(
+                                      child: RichText(
+                                        overflow: TextOverflow.ellipsis,
+                                        text: TextSpan(
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 12,
+                                                fontWeight: widget.messageRead
+                                                    ? FontWeight.bold
+                                                    : FontWeight.normal),
+                                            text: widget.messageContent
+                                                as String),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ]),
+                      )),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Text(
-              widget.time.toString(),
-              style: GoogleFonts.cabin(
-                fontSize: 12,
-                color: Colors.white,
-                fontWeight: widget.messageRead ? FontWeight.bold : FontWeight.normal
-              ),
-            )
-          ],
-        )
-      )
-    );
+                ),
+                Text(
+                  widget.time.toString(),
+                  style: GoogleFonts.cabin(
+                      fontSize: 12,
+                      color: Colors.black,
+                      fontWeight: widget.messageRead
+                          ? FontWeight.bold
+                          : FontWeight.normal),
+                )
+              ],
+            )));
   }
 
   void createConversation(BuildContext context) {
     String convoID = Helpers.getConvoID(widget.uid, widget.pid);
     Navigator.of(context).push(MaterialPageRoute(
-      builder: (BuildContext context) => ChatDetailView(
-        uid: widget.uid, 
-        contact: widget.contact, 
-        convoID: convoID,
-        photoURL: widget.imageUrl ?? 'https://i.pravatar.cc/150?img=1',
-      )));
+        builder: (BuildContext context) => ChatDetailView(
+              uid: widget.uid,
+              contact: widget.contact,
+              convoID: convoID,
+              photoURL: widget.imageUrl ?? 'https://i.pravatar.cc/150?img=1',
+            )));
   }
 }
